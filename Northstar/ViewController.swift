e05b4dc378c318b4ba9d58c65d57598e7f858aaf
Daxit Agarwal <Daxit.Agarwal@ibm.com>
2019-06-05 11:50:01 -0700

    Initial Commit

---
+++ Northstar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Northstar
+//
+//  Created by Daxit Agarwal on 6/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
